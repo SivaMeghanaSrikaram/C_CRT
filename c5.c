3e41a128933bbcdcbc317d5ab4e214e51c88eba6
Meghana <228A1A05A1@risekrishnasaiprakasam.edu.in>
2025-02-19 09:17:56 +0530

    Add files via upload

---
+++ c5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+	bool a=35;
+	bool b=0;
+	printf("%d\n",a>b);
+	printf("%d\n",a>=b);
+	printf("%d\n",a<b);
+	printf("%d\n",a<=b);
+	printf("%d\n",a==b);
+	printf("%d\n",a!=b);
+	return 0;
+}
